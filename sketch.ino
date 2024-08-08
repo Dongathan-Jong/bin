67ca8782e9203c1fa91112645279cf24a260820a
Dongathan-Jong <duclingsyt@gmail.com>
2024-08-08 10:33:32 -0400

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,4 +1,3 @@
-// Now turn this trash into treasure!
 #include <TM1637.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -8,6 +7,14 @@
 #define neopixel 5
 #define clk 27
 #define dio 28
+
+bool alarmTriggered = false;
+unsigned long alarmStartTime = 0;
+int passcode[] = {3, 1, 4};
+int enteredCode[3] = {0, 0, 0};
+int codeIndex = 0;
+unsigned long lastButtonPressTime = 0;
+const unsigned long buttonDebounceDelay = 200;
 
 TM1637 tm(clk, dio);
 Adafruit_NeoPixel pixels(1, neopixel, NEO_GRB + NEO_KHZ800);
@@ -20,7 +27,64 @@ void setup() {
   tm.init();
   tm.set(BRIGHT_TYPICAL);
   pixels.begin();
+  pixels.clear();
 }
-void loop() {
 
+void loop() {
+  if (digitalRead(pir) == HIGH) {
+    if (!alarmTriggered) {
+      alarmTriggered = true;
+      alarmStartTime = millis();
+    }
+  }
+
+  if (alarmTriggered) {
+    digitalWrite(buzzer, HIGH);
+    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+    pixels.show();
+    
+    unsigned long elapsedSeconds = (millis() - alarmStartTime) / 1000;
+    tm.display(0, (elapsedSeconds / 10) % 10);
+    tm.display(1, elapsedSeconds % 10);
+  }
+
+  if (digitalRead(button) == LOW && millis() - lastButtonPressTime > buttonDebounceDelay) {
+    lastButtonPressTime = millis();
+    enteredCode[codeIndex]++;
+    tm.display(codeIndex, enteredCode[codeIndex]);
+  }
+
+  if (digitalRead(button) == HIGH && millis() - lastButtonPressTime > 1000 && enteredCode[codeIndex] > 0) {
+    lastButtonPressTime = millis();
+    codeIndex++;
+  }
+
+  if (codeIndex >= 3) {
+    if (checkPasscode()) {
+      alarmTriggered = false;
+      digitalWrite(buzzer, LOW);
+      pixels.clear();
+      pixels.show();
+      tm.clearDisplay();
+    } else {
+      resetEnteredCode();
+    }
+  }
+}
+
+bool checkPasscode() {
+  for (int i = 0; i < 3; i++) {
+    if (enteredCode[i] != passcode[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void resetEnteredCode() {
+  for (int i = 0; i < 3; i++) {
+    enteredCode[i] = 0;
+  }
+  codeIndex = 0;
+  tm.clearDisplay();
 }
